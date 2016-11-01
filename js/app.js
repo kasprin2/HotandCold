381e8879929b53f718c85ca63a8e61a7889847c7
Kate Springsteen <katherine.springsteen@gmail.com>
2016-10-31 18:19:29 -0600

    Updates

--- js/app.js
+++ js/app.js
@@ -1,15 +1,10 @@
-//creating variable for the randomNumber
+// creating variable for the randomNumber
 var randomNumber = 0;
 
-//creating the variable for the difference between randomNumber and user's input
-//function for difference (response) is below
-
-
-
-var count = 0;
+var counter = 0;
 
 $(document).ready(function() {
-	//calling the function randomizeNumber();  / creating a random number for the page
+	// calling the function randomizeNumber();  / creating a random number for the page
 	randomizeNumber();
 	
 	/*--- Display information modal box ---*/
@@ -22,26 +17,32 @@ $(document).ready(function() {
   		$(".overlay").fadeOut(1000);
   	});
 
-	//convert string to number, list guess, add in counter, compare, output
+  	$('.new').click(function() {
+  		restart();
+		counter = 0;
+		randomizeNumber();
+  		});
+
+	// convert string to number, list guess, add in counter, compare, output
 	$("#guessButton").click(function(e) {
 		e.preventDefault();
-		//converting string to number
+		// converting string to number
 		input = $("#userGuess").val();
 		parseInt(input);
-		//validate if input is blank or not a number. If either one below is true nothing else below function will be executed (return) and "Enter a number" will be listed.
+		// validate if input is blank or not a number. If either one below is true nothing else below function will be executed (return) and "Enter a number" will be listed.
 		if (!input || isNaN(input)) {
 			$('#feedback').text("Enter a number");
 			return;
 		}
-		//list number in <li>
-		$('#guessList li:first').append("<li>" + input + "</li>");
-		//calling the response function to compare input and randomNumber (see var difference) and display feedback
-		response();
-		//returning the userGuess input section to blank
+		// list number in <li>
+		$('#guessList').append("<li class='guess'>" + input + "</li>");
+		// calling the response function to compare input and randomNumber (see var difference) and display feedback
+		respond();
+		// returning the userGuess input section to blank
 		$("#userGuess").val('');
-		//guess count
-		counter();
-	})
+		// guess count
+		count();
+	});
 });
 
 function randomizeNumber() {
@@ -49,10 +50,8 @@ function randomizeNumber() {
 	console.log("randomNumber: " + randomNumber);
 }
 
-function response() {
-	console.log(input, "res");
+function respond() {
 	var difference = Math.abs(randomNumber - input);
-	console.log(difference);
 	
 	if (difference >= 50) {
 		$('#feedback').text('Freezing');
@@ -74,25 +73,15 @@ function response() {
 	}
 };
 
-function counter(){
-			count += 1;
-  			$('#count').text(count);
+function count() {
+			counter += 1;
+  			$('#count').text(counter);
 };
 
-//when page loads, load a new game
+function restart() {
+	$('#feedback').text('Make your Guess!');
+	$('li.guess').remove();
+	$('#count').text('0');
+};
 
-//create a random number when a new game is started
 
-//allow a new game to be started without a page refresh
-
-//give user feedback on guess
-
-//supply user with list of guesses supplied so f
-
-//events (1) new game (reload, refresh, hitting new button) random number created
-
-//events (2) user hits submit guess - ensures that guess is a number, 
-//converts string to number
-//lists number in <li>
-//compares number to random number
-//based on comparison decides which output to give - hot, cold, colder, hotter, warm, cool, Right On!
